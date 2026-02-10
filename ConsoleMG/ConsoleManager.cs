16c185faea240760f0e26a044bdf69ac9bccbf9a
Hycore Progarmming <hoangbussines@gmail.com>
2026-02-10 18:02:57 +0700

    Add files via upload

---
+++ ConsoleMG/ConsoleManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Hycore.ConsoleMG
+{
+    public static class ConsoleManager
+    {
+        public static void WriteLine()
+        {
+            Console.WriteLine();
+        }
+
+        public static void WriteLine(string value)
+        {
+            Console.WriteLine(value);
+        }
+
+        public static void WriteLine(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+
+        public static void Write(string value)
+        {
+            Console.Write(value);
+        }
+
+        public static void Write(string format, params object[] args)
+        {
+            Console.Write(format, args);
+        }
+    }
+}
